d8123256b5a292b8c879c863ae0aca9f902f1583
davidhartman00 <davidhartman00@gmail.com>
2019-07-04 13:25:05 -0700

    files updated

--- react-hamburger/src/components/Layout/Layout.js
+++ react-hamburger/src/components/Layout/Layout.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import Aux from "../../hoc/Aux";
 
 const layout = () => {
+  <Aux>
     <div> Toolbar, SideDrawer, Backdrop</div>
-    <main>
-        {props.childern}
-    </main>
-}
+    <main>{props.childern}</main>
+  </Aux>;
+};
+
+export default App;
---
+++ react-hamburger/src/hoc/Aux.js
@@ -0,0 +1,5 @@
+// As this is just a auxilery component that is used only to 
+// us as a wrapper component, we do not need to import React
+
+const aux = () => props.children;
+export default aux
