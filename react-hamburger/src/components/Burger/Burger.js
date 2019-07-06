80e8648cf8ac0fb5b49f6753ed4f925b3b02218c
davidhartman00 <davidhartman00@gmail.com>
2019-07-05 20:55:45 -0700

    files renamed and changes

---
+++ react-hamburger/src/components/Burger/Burger.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from "./Burger.css"
+
+const burger = (props) =>{
+    return (
+        <div className={style.module.Burger}></div>
+    );
+}
+
+export default burger;
