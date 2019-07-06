dc08d122aadfb48f7974ea7d8ee78508ba1a016d
davidhartman00 <davidhartman00@gmail.com>
2019-07-06 14:33:31 -0700

    added proptype checking

--- react-hamburger/src/components/Burger/Burger.js
+++ react-hamburger/src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import style from "./Burger.module.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) =>{
+    const transformedIngredients = Object.keys(props.ingredients)//pulled out the keys names into an array
+        .map(igKey =>{ //loop over key titles
+            return [...Array(props.ingredients[igKey])].map((_,i) => { //return an array of the quantity of each key, then map over each
+                 return <BurgerIngredient key={igKey + i } type={igKey} /> //build an array of JSX to return
+            })
+        })
     return (
         <div className={style.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
