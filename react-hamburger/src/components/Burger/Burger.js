20456f8cf970eb6cc15249b7ad7756342b5dc01d
davidhartman00 <davidhartman00@gmail.com>
2019-07-06 14:51:29 -0700

    checked burger to dynamic building

--- react-hamburger/src/components/Burger/Burger.js
+++ react-hamburger/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import style from "./Burger.module.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) =>{
-    const transformedIngredients = Object.keys(props.ingredients)//pulled out the keys names into an array
+    let transformedIngredients = Object.keys(props.ingredients)//pulled out the keys names into an array
         .map(igKey =>{ //loop over key titles
             return [...Array(props.ingredients[igKey])].map((_,i) => { //return an array of the quantity of each key, then map over each
                  return <BurgerIngredient key={igKey + i } type={igKey} /> //build an array of JSX to return
             })
-        })
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+        // console.log(transformedIngredients);
+    if (transformedIngredients.length === 0 ){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }        
+
     return (
         <div className={style.Burger}>
             <BurgerIngredient type="bread-top" />
