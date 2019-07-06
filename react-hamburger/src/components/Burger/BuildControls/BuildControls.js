0fb15a45364a6954658fe43afda9e82537d2ffb0
davidhartman00 <davidhartman00@gmail.com>
2019-07-06 15:17:40 -0700

    css and build control added

---
+++ react-hamburger/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import style from './BuildControl.module.css'
+const buildControls = (props) => (
+    
+        <div>
+            
+        </div>
+    
+)
+
+export default buildControls
