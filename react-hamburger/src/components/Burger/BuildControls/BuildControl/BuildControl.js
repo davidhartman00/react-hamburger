429ed742d824f2e29567c9b2aa8ed946f5807adf
davidhartman00 <davidhartman00@gmail.com>
2019-07-07 22:08:39 -0700

    hooked up onclick

--- react-hamburger/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ react-hamburger/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import style from './BuildControl.module.css'
 const buildControl = props => (
   <div className={style.BuildControl}>
     <div className={style.Label}>{props.label}</div>
-    <button className={style.Less}>Less</button>
+    <button className={style.Less} onClick={props.remove}>Less</button>
     <button className={style.More} onClick={props.added}>More</button>
   </div>
 );
