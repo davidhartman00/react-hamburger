ec79becea00df996ddaa65bcfac5e6779384876a
davidhartman00 <davidhartman00@gmail.com>
2019-07-09 21:55:21 -0700

    added new method

--- react-hamburger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-hamburger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,20 @@ class BurgerBuilder extends Component {
     purchaseable: false
   };
 
+  updatePurchaseState(){
+    const ingredients = {
+      ...this.state.ingredients
+    }
+    const sum = Object.keys(ingredients)
+    .map(igKey => {
+      return ingredients[igKey]
+    })
+    .reduce((sum, el) => {
+      return sum + el
+    },0)
+    this.setState({purchaseable: sum > 0})
+  }
+
   addIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -77,6 +91,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchaseable = {this.state.purchaseable}
           price={this.state.totalPrice}
         />
       </Aux>
