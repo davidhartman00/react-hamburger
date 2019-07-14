894f10344df38e74ce3c4c8a6593e3f5c2e48aaa
davidhartman00 <davidhartman00@gmail.com>
2019-07-14 15:08:22 -0700

    added modal to burger builder

--- react-hamburger/src/components/UI/Modal/Modal.js
+++ react-hamburger/src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import style from "./Modal.css"
+import style from "./Modal.module.css"
 
 const modal = props => <div className={style.Modal}>{props.children}</div>;
 
--- react-hamburger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-hamburger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuilderControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from '../../
+components/UI/Modal/Modal'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -85,6 +87,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal />
         <Burger ingredients={this.state.ingredients} />
         <BuilderControls
           ingredientAdded={this.addIngredientHandler}
