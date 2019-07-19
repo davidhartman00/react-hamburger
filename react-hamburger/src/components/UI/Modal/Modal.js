2e776495252db82dc527568eda968e2f2f0d8a5d
davidhartman00 <davidhartman00@gmail.com>
2019-07-18 23:54:03 -0700

    small change

--- react-hamburger/src/components/UI/Modal/Modal.js
+++ react-hamburger/src/components/UI/Modal/Modal.js
@@ -5,7 +5,8 @@ import style from "./Modal.module.css";
 const modal = props =>(
         <div className={style.Modal}
         style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? '1' : '0'
         }}>
             {props.children}
         </div>
