f1d4033140d2cb6c3257af45cf0fd7855f102efd
davidhartman00 <davidhartman00@gmail.com>
2019-07-21 00:35:49 -0400

    added Aux to Modal

--- react-hamburger/src/components/UI/Modal/Modal.js
+++ react-hamburger/src/components/UI/Modal/Modal.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 import style from "./Modal.module.css";
+import Aux from '../../../hoc/Aux'
 
 const modal = props =>(
+    <Aux>
+        <Backdrop show={props.show}></Backdrop>
         <div className={style.Modal}
         style={{
             transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
@@ -11,5 +14,5 @@ const modal = props =>(
             {props.children}
         </div>
     );
-
+</Aux>
 export default modal;
