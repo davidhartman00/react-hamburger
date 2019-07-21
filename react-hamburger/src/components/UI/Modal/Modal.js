fef580365b86bf946a315ad1e289f0a727ba3e50
davidhartman00 <davidhartman00@gmail.com>
2019-07-21 00:40:11 -0400

    formatting

--- react-hamburger/src/components/UI/Modal/Modal.js
+++ react-hamburger/src/components/UI/Modal/Modal.js
@@ -1,19 +1,21 @@
 import React from "react";
 
 import style from "./Modal.module.css";
-import Aux from '../../../hoc/Aux'
-import Backdrop from '../Backdrop/Backdrop'
+import Aux from "../../../hoc/Aux";
+import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props =>(
-    <Aux>
-            <Backdrop show={props.show}></Backdrop>
-        <div className={style.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-</Aux>
-        );
+const modal = props => (
+  <Aux>
+    <Backdrop show={props.show} />
+    <div
+      className={style.Modal}
+      style={{
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0"
+      }}
+    >
+      {props.children}
+    </div>
+  </Aux>
+);
 export default modal;
