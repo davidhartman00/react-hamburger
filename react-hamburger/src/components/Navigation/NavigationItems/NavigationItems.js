44260cca893096ae1b6871d6676b9cf648fddb32
davidhartman00 <davidhartman00@gmail.com>
2019-07-23 22:33:51 -0700

    hook up NavigationItem

--- react-hamburger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ react-hamburger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import style from './NavigationItems.module.css'
+import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = (props) => (
 <ul className={style.NavigationItems}>
+    <NavigationItem />
 </ul>
 )
 
