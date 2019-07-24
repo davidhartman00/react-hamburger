205652cdb5a4cf7b3899f0e03142d81a45696cbc
davidhartman00 <davidhartman00@gmail.com>
2019-07-23 21:45:53 -0700

    import toolbar to layout

--- react-hamburger/src/components/Layout/Layout.js
+++ react-hamburger/src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import Aux from "../../hoc/Aux";
 import style from "./Layout.module.css";
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = props => (
   <Aux>
-    <div> Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={style.Content}>{props.children}</main>
   </Aux>
 );
