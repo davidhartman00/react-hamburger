e579e73b766bb9c89166726157173695ad010aea
davidhartman00 <davidhartman00@gmail.com>
2019-07-23 22:43:52 -0700

    formatting

--- react-hamburger/src/components/Navigation/Toolbar/Toolbar.js
+++ react-hamburger/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import style from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={style.Toolbar}>
     <div>MENU</div>
     <Logo />
-    <nav>...</nav>
+    <NavigationItems />
   </header>
 );
 
