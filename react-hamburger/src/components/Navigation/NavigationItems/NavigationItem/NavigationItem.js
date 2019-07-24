3d866a0174abc6f2b7db53ad8c692a14cece2cb9
davidhartman00 <davidhartman00@gmail.com>
2019-07-23 22:44:54 -0700

    formatted NavigationItems and NavigationItem

--- react-hamburger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ react-hamburger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-import style from './NavigationItem.module.css'
+import style from "./NavigationItem.module.css";
 
 const navigationItem = props => (
   <li className={style.NavigationItem}>
-    <a 
-    href={props.link} 
-    className={props.active ? style.active : null }>
-    {props.children}</a>
+    <a href={props.link} className={props.active ? style.active : null}>
+      {props.children}
+    </a>
   </li>
 );
 
--- react-hamburger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ react-hamburger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import style from './NavigationItems.module.css'
-import NavigationItem from './NavigationItem/NavigationItem'
+import style from "./NavigationItems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = (props) => (
-<ul className={style.NavigationItems}>
-    <NavigationItem link='/' active>Burger Builder</NavigationItem>
-    <NavigationItem link='/'>Checkout</NavigationItem>
-</ul>
-)
+const navigationItems = props => (
+  <ul className={style.NavigationItems}>
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+);
 
-export default navigationItems
+export default navigationItems;
