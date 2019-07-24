de8d4a54f760ff87de8cbd1775d10d564c4796dd
davidhartman00 <davidhartman00@gmail.com>
2019-07-23 21:56:39 -0700

    new cmoponent

---
+++ react-hamburger/src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import burgerLogo from '../../assets/images/burger-logo.png'
+
+const logo = props => (
+  <div>
+    <img scr={burgerLogo} alt="myBurger"/>
+  </div>
+);
+
+export default logo;
