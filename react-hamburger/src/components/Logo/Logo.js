91d24f002c7c2e940a17f701c442b7d09e77a88a
davidhartman00 <davidhartman00@gmail.com>
2019-07-29 22:44:47 -0700

    logo  height mondification

--- react-hamburger/src/components/Logo/Logo.js
+++ react-hamburger/src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from "../../assets/images/burger-logo.png";
 import style from "./Logo.module.css";
 
 const logo = props => (
-  <div className={style.Logo}>
+  <div className={style.Logo} style={{height: props.height}}>
     <img src={burgerLogo} alt="myBurger" />
   </div>
 );
