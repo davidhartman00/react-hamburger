da493186814fe6c8e9ab4b8123a8e0ca57ec4538
davidhartman00 <davidhartman00@gmail.com>
2019-07-29 22:29:27 -0700

    start of sideDrawer  again

--- react-hamburger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react-hamburger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const sideDrawer = props => {
 // .. conditionsally do some css here.
 
     return(
-
+        <div>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
     )
 }
 
-export default sideDrawer
+export default sideDrawer;
