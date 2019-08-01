65682bcf06ae76621447cf79980b45b7845da35f
davidhartman00 <davidhartman00@gmail.com>
2019-07-31 21:40:48 -0700

    changes to side drawer

--- react-hamburger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react-hamburger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,19 +3,24 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import style from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
 const sideDrawer = props => {
   // .. conditionsally do some css here.
 
   return (
-    <div className={style.SideDrawer}>
-      <div className={style.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show/>
+      <div className={style.SideDrawer}>
+        <div className={style.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
