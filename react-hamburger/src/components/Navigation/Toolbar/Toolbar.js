58baa2f499398714f2536d894a7477b29ba6113f
davidhartman00 <davidhartman00@gmail.com>
2019-08-04 14:24:56 -0700

    change how drawer toggling is handled

--- react-hamburger/src/components/Navigation/Toolbar/Toolbar.js
+++ react-hamburger/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import React from "react";
 import style from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import Menu from "../Menu/Menu"
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
+
 
 const toolbar = props => (
   <header className={style.Toolbar}>
-    <Menu openSideDrawer={props.openSideDrawer} />
+      <DrawerToggle />
     <div className={style.Logo}>
       <Logo />
     </div>
