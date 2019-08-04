577b98611e5d799cba2a145f64cc40ab35b5f381
davidhartman00 <davidhartman00@gmail.com>
2019-08-04 15:15:47 -0700

    hook up new css

--- react-hamburger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ react-hamburger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import style from './DrawerToggle.module.css'
 
 const drawerToggle = (props) => (
-    <div onClick={props.openSideDrawer}>MENU</div>
+    <div onClick={props.openSideDrawer} className={style.DrawerToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
     )
 
 export default drawerToggle
